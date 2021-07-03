4a029a19d91be8db5c3e452414d945820cd78368
“Younis” <younisrahman007@gmail.com>
2021-07-04 01:51:54 +0600

    Secound Class

---
+++ class2topic.js
@@ -0,0 +1,49 @@
+// Custome component Start with Capital
+
+
+
+// style comppnent 
+
+// const imageContainerStyle = {
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+// };
+
+
+
+
+
+// State 
+// State use for component internal data change 
+// and re render React dom
+// in store we save dynamic data
+
+// ex 1
+// constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     };
+//   }
+
+//   ex 2
+
+//   state ={
+
+//   }
+
+
+
+
+//   more about function 
+//   onClick={this.functionname}
+//   onClick={()=>this.functionname()}
+//   onClick={this.functionname.bind(this,"perameter")}
+
+
+
+
+//   fetch from online
+
+//   componentdid mount
--- src/App.js
+++ src/App.js
@@ -1,169 +1,71 @@
 import './App.css';
 import React from 'react';
 
-// Next Clases :
-
-// 1.State
-// 2.Route
-
-// 3.Lifecycle methods
-
-// 4.Higher Order Components
-// 5.Context API 
-
-
-class Programmer extends React.Component {
-
-    render(){
-      console.log(this.props)
-      return (
-        <div>
-          <h1 className="asu">{this.props.name}</h1>
-          <h2>{this.props.role}</h2>
-          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-            optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-            recusandae alias error harum maxime adipisci amet lab</h3>
-        </div>
-      )
-    }
-  }
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: {
+        innerName: "Name",
+        nextName: "name"
+      },
+      id : 1
+    };
+    console.log("Constructor")
+  }
 
-  // Func1=(props)=>{
-  //   return(
-  //     <h1>
-  //       Hello
-  //     </h1>
-  //   )
+  // state = {
+  //   name: 'Naim'
   // }
-  
+
+
+  componentDidMount() {
+    fetch(`https://jsonplaceholder.typicode.com/users/${this.state.id}`).
+      then(res => res.json()).
+      then(data => {
+        console.log(data)
+        this.setState({
+          name: { ...this.state.name, innerName: data.name }
+        })
+      })
+  }
+
+
+
+  changeName() {
+    this.setState({
+      id : 2
+    })
+    fetch(`https://jsonplaceholder.typicode.com/users/${this.state.id}`).
+      then(res => res.json()).
+      then(data => {
+        console.log(data)
+        this.setState({
+          name: { ...this.state.name, innerName: data.name }
+        })
+      })
+  }
+
+
+
   render() {
+    console.log("After Constructor")
+
     return (
-      
       <div className="App">
-        {/* {this.Func1(name="asu",role='DEv')} */}
-        <Programmer name="Asu" role="Hacker" />
-        <Programmer name="Younis" role="Mobile Developer" />
-        <Programmer name="Naim" role="Web Developer" />
+        <h1 style={nameStyle}>{`Hello this is : ${this.state.name.innerName}`}</h1>
+        <button onClick={() => this.changeName()}> Change Name</button>
       </div>
     );
   }
 }
 
+
+const nameStyle = {
+  fontSize: 50,
+}
+
 export default App;
 
-
-
-
-
-
-// class Programmer extends React.Component {
-  
-//   render(){
-//     console.log(this.props)
-//     return (
-//       <div>
-//         <h1>{this.props.name}</h1>
-//         <h2>{this.props.role}</h2>
-//         <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-//           molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-//           numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-//           optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-//           obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-//           nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-//           tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-//           quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-//           sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-//           recusandae alias error harum maxime adipisci amet lab</h3>
-//       </div>
-//     )
-//   }
-// }
-
-// const Programmer = (props) => {
-//   return (
-//     <div>
-//       <h1>{props.name}</h1>
-//       <h2>{props.role}</h2>
-//       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-//         molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-//         numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-//         optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-//         obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-//         nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-//         tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-//         quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-//         sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-//         recusandae alias error harum maxime adipisci amet lab</h3>
-//     </div>
-//   )
-// }
-// const Funnc4 = () => (
-//   <h1>
-//     Hello
-//   </h1>
-// )
-
-// const Func1 = () => {
-//   let a = 5
-//   let b = 5
-//   let c = a + b
-
-//   return (
-//     <h1>{c}</h1>
-//   )
-
-// }
-// class App extends React.Component {
-
-//   // const todo = ['eat', 'code', 'sleep']
-
-//   // const data = todo.map(item => {
-//   //   return (
-//   //     <h1>
-//   //       {item}
-//   //     </h1>
-//   //   )
-//   // })
-
-
-
-//   render() {
-
-//     return (
-//       <div className="App">
-//         {/* {this.Func1()} */}
-
-//         < Func1 />
-
-//         < Programmer name="Asu" role="Hacker" />
-//         < Programmer name="Younis" role="Mobile Developer" />
-//         < Programmer name="Naim" role="Web Developer" />
-//         {/* <h1>
-//           {todo.map((attribute, index) => (
-//             <h1>{ index }</h1>
-//           ))}
-//         </h1> */}
-//         {/* <h2>{todo.map(item => {
-//           return (
-//             <h1>
-//               {item}
-//             </h1>
-//           )
-//         })}</h2> */}
-//       </div>
-//     );
-//   }
-
-// }
-
-// export default App;
